3ac538a5811b59abb131bcfd650cbe42119e96a8
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-14 13:24:53 -0300

    finalização da aula

---
+++ cursojs/aula13/ambiente.js
@@ -0,0 +1,11 @@
+// var c = 1
+// while(c <= 1500){
+//     console.log(`${c}° Tudo Bem?`)
+//     c ++
+// }
+var c =  1
+do {
+    console.log(`${c}° Tudo Bem?`)
+    c++
+}
+while (c <=6)
---
+++ cursojs/aula14/ambiente.js
@@ -0,0 +1,5 @@
+// var c = 1
+for (var c = 1; c <= 4; c++){
+    console.log(c)
+}
+console.log('Fim')
