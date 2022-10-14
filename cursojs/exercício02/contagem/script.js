59f2feeb1c9a4dcbf73f7517ceea75fafae553a1
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-14 18:30:32 -0300

    Exercício 2

---
+++ cursojs/exercício02/contagem/script.js
@@ -0,0 +1,7 @@
+var a = Number(document.getElementById('a').value)
+var b = Number(document.getElementById('b').value)
+var c = Number(document.getElementById('c').value)
+var div = document.getElementById('resultado')
+function Contar(){
+    
+}
