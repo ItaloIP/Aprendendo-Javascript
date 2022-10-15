3e45dabe3ff3ab052b02a185165b464144a51af7
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-15 17:26:31 -0300

    Tentando resolver o exercício

--- cursojs/exercício02/contagem/script.js
+++ cursojs/exercício02/contagem/script.js
@@ -1,7 +1,15 @@
-var a = Number(document.getElementById('a').value)
-var b = Number(document.getElementById('b').value)
-var c = Number(document.getElementById('c').value)
-var div = document.getElementById('resultado')
 function Contar(){
-    
+    var a = Number(document.getElementById('a').value)
+    var b = Number(document.getElementById('b').value)
+    var c = Number(document.getElementById('c').value)
+    var div = document.getElementById('resultado')
+    if (c == 0){
+        alert('Pula será considerado 1.')
+        var c = 1
+    }
+
+    for (a; a <=b ; a++){
+        div.innerText= `${a}`
+        // div.innerHTML = 
+    }
 }
