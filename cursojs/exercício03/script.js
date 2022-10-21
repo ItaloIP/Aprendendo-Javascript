98a4a67d9d803ea8e10fffb23eab8b5d39cec3b5
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-21 15:43:13 -0300

    Finalizado o exercício

--- cursojs/exercício03/script.js
+++ cursojs/exercício03/script.js
@@ -1,15 +1,24 @@
 let numero = []
+var p = document.getElementById('resultado')
 
 function Adicionar(){
     var n = Number(document.getElementById('num').value)
+    var input = document.getElementById('num')
     if (n <= 0 || n > 100 ){
-        alert('Adicione um número correto ou não deixe o espaço em branco')
-    }else{
+        alert('Adicione um número correto ou não deixe o espaço em branco')}
+    else if (numero.indexOf(n) != -1) {
+        alert('Valor repetido! Escolha outro.')
+    }
+    else{
         var resul = document.getElementById('resul')
         numero.push(n)
         let item = document.createElement('option')
         item.innerText = `${n} adicionado na lista.`
-        resul.appendChild(item)}
+        p.innerHTML = ''
+        resul.appendChild(item)
+    }
+    input.value = ''
+    input.focus()
 }
 
 function Maior(){
@@ -29,8 +38,29 @@ function Menor(){
     return menor
 } 
 
+function Media(){
+    let media = 0
+    for (var num in numero){
+        media += numero[num]}
+    return (media / numero.length)
+}
+
+function Soma(){
+    let soma = 0
+    for (var num in numero){
+        soma += numero[num]}
+    return soma
+}
+
 function Finalizar(){
-    var p = document.getElementById('resultado')
-    p.innerHTML += `O maior número é o ${Maior()} <br>`
-    p.innerHTML += `O menor número é o ${Menor()} <br>`
+    if (numero.length == 0) {
+        alert('Adicione números!')
+    }
+    else{
+        p.innerHTML = ''
+        p.innerHTML += `<p>Há ${numero.length} números registrados. <br></p>`
+        p.innerHTML += `<p>O maior número é o ${Maior()} <br></p>`
+        p.innerHTML += `<p>O menor número é o ${Menor()} <br></p>`
+        p.innerHTML += `<p>A média é ${Media()} <br></p>`
+        p.innerHTML += `<p>A soma de todos os números é ${Soma()}</p>`}
 }
