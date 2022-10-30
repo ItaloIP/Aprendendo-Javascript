10b5912db52038598ce54bf8673e3cadeee74da8
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-10-30 14:18:17 -0300

    Create script.js

---
+++ rocketseat/desafios/convertertemp/script.js
@@ -0,0 +1,12 @@
+function TempConverter(txt){
+    let text = txt.toUpperCase
+    let num = 0
+    let temp = ''
+    for (let letra of txt){
+        if (letra == Number){
+            let += letra
+        }
+    }
+}
+
+console.log(TempConverter('45f'))
