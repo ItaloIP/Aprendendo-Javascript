3e658570ea1b86f9b58499fc393888328983845f
Italo Pereira Vieira <101375275+ItaloIP@users.noreply.github.com>
2022-11-03 19:11:20 -0300

    Fazendo exercício

---
+++ rocketseat/captura_ no_documento/script.js
@@ -0,0 +1,5 @@
+const botão = document.querySelector('#OpenModal')
+const Jan = document.querySelector('.modal-wrapper').classList.contains
+function AbrirJan(){
+    Jan.innerText -= 'invisible'
+}
